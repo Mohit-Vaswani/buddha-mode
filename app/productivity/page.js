975b43a75ff3b11530492088fcbef7c56ce11cdf
Mohit <mohitvaswani010@gmail.com>
2023-09-16 16:59:54 +0530

    metadata updated

--- app/productivity/page.js
+++ app/productivity/page.js
@@ -1,6 +1,11 @@
 import AllFeatures from "@/components/Productivity/AllFeatures"
 import MainHeader from "@/components/crafts/MainHeader"
 
+export const metadata = {
+  title: 'Productivity',
+  description: 'A simple yet powerful productivity tool.',
+};
+
 const page = () => {
   return (
     <section className="w-full h-screen p-10">
